fccb6e27f2024e0635a6af19474beb207241c42b
Giorgio Garofalo <giogar2705@gmail.com>
2024-01-12 13:20:30 +0100

    Toggle watched/unwatched and watchlist

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggleClass(element, className) {
+    if (element.classList.contains(className)) {
+        element.classList.remove(className);
+    } else {
+        element.classList.add(className);
+    }
+}
