5cf5e7ba35aa83df45eefb08bc75a50283819b33
Giorgio Garofalo <giogar2705@gmail.com>
2024-01-13 16:37:19 +0100

    Add profile template

---
+++ profile.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once("bootstrap.php");
+
+$profile = array(
+    "username" => "tizio",
+    "bio" => "Lorem ipsum dolor sit amet consectetur. Velit in morbi aliquet scelerisque nec fringilla diam tellus sit.
+    Gravida sed convallis orci bibendum. Vehicula tellus volutpat habitant convallis egestas ac arcu. A
+    dictumst risus scelerisque nulla quis.",
+); // TODO get from db
+
+$template["title"] = $profile["username"];
+$template["content"] = "profile_content.php";
+$template["profile"] = $profile;
+
+require("template/base.php");
+
+?>
---
+++ template/profile_content.php
@@ -0,0 +1,75 @@
+<?php
+
+$profile = $template["profile"];
+if (!isset($profile)) {
+    die("Profile not set");
+}
+
+?>
+
+<section class="col-left">
+    <header>
+        <h1><?php echo $profile["username"] ?></h1>
+        <a class="follow" href="#" onclick="toggleClass(this, 'following', 'Following', 'Follow')">Follow</a>
+    </header>
+    <div class="bio">
+        <p><?php echo $profile["bio"] ?></p>
+        <p><strong>10</strong> followers</p>
+        <p><strong>10</strong> seguiti</p>
+    </div>
+    <section>
+        <h2>Visti</h2>
+        <ul class="movie-list">
+            <li>
+                <a href="#">
+                    <div></div>
+                    <p>Titolo Film</p>
+                </a>
+            </li>
+            <li>
+                <div></div>
+                <p>Titolo Film</p>
+            </li>
+            <li>
+                <div></div>
+                <p>Titolo Film</p>
+            </li>
+            <li>
+                <div></div>
+                <p>Titolo Film</p>
+            </li>
+            <li>
+                <div></div>
+                <p>Titolo Film</p>
+            </li>
+        </ul>
+    </section>
+    <section>
+        <h2>Da vedere</h2>
+        <ul class="movie-list">
+            <li>
+                <div></div>
+                <p>Titolo Film</p>
+            </li>
+            <li>
+                <div></div>
+                <p>Titolo Film</p>
+            </li>
+            <li>
+                <div></div>
+                <p>Titolo Film</p>
+            </li>
+            <li>
+                <div></div>
+                <p>Titolo Film</p>
+            </li>
+            <li>
+                <div></div>
+                <p>Titolo Film</p>
+            </li>
+        </ul>
+    </section>
+</section>
+<section class="col-right">
+    <h2>Post</h2>
+</section>
