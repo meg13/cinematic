e161af641e50bb702a77b6a2700faa20b2654965
Giorgio Garofalo <giogar2705@gmail.com>
2024-01-16 00:47:43 +0100

    Add followed watch count for movie

--- movie.php
+++ movie.php
@@ -21,6 +21,7 @@ $template["content"] = "movie_content.php";
 $template["movie"] = $movie;
 $template["in_watchlist"] = $dbh->isInWatchlist(get_logged_in_username(), $id);
 $template["in_watched"] = $dbh->isWatched(get_logged_in_username(), $id);
+$template["followed_watched_count"] = $dbh->getNumberOfUserHaveSeenMovie(get_logged_in_username(), $id);
 
 require("template/base.php");
 
