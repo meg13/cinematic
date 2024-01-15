d69e678c540cc0e91d114fd2ab42f49a711310bf
Giorgio Garofalo <giogar2705@gmail.com>
2024-01-15 02:09:25 +0100

    Autofocus search field

--- template/search_content.php
+++ template/search_content.php
@@ -1,9 +1,6 @@
 <?php
 
 $movies = $template["movies"];
-if (!isset($movies)) {
-    die("Movies data not set.");
-}
 
 ?>
 
@@ -13,12 +10,12 @@ if (!isset($movies)) {
     </header>
     <form action="#">
         <label for="s" hidden>Cerca</label>
-        <input type="text" id="s" name="s" placeholder="Cerca un film..." rows="1"></input>
+        <input type="text" id="s" name="s" placeholder="Cerca un film..." rows="1" autofocus <?php if (isset($query)) echo 'value="' . $query . '"' ?> onfocus="moveCursorToEnd(this)"></input>
         <input type="submit" value="Cerca">
     </form>
     <ul class="movie-list movie-grid">
         <?php
-        if (isset($query)) {
+        if (isset($movies)) {
             foreach ($movies as $movie) {
                 $_GET["movie"] = $movie;
                 require("movie_element.php");
