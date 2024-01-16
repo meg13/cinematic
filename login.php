87aac5248ee99a56f3a56d8f0916b925b2615f1f
Margherita Raponi <raponi.margherita.rm@gmail.com>
2024-01-16 22:45:35 +0100

    added php for login

---
+++ login.php
@@ -0,0 +1,13 @@
+<form action="#" method="POST">
+    <ul>
+        <li><label for="email">E-mail</label>
+            <input type="text" id="email" name="email" placeholder="email@example.com"/>
+        </li>
+        <li><label for="password">Password</label>
+            <input type="password" id="password" class="wrong" name="password"/>
+        </li>
+        <li>
+            <input type="submit" value="Accedi"/>
+        </li>
+    </ul>
+</form>
