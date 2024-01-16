4b35aca4ed87210a329c1f042f5e05729d8b02cc
Giorgio Garofalo <giogar2705@gmail.com>
2024-01-16 16:13:32 +0100

    Add user search

--- search.php
+++ search.php
@@ -6,8 +6,8 @@ $template["title"] = "Ricerca";
 $template["content"] = "search_content.php";
 
 $query = $_GET["s"];
-$template["users"] = array("user1", "user2", "user3"); // TODO query
-$template["movies"] = omdb_search_movies_by_title($query);
+$template["users"] = isset($query) ? array_column($dbh->searchUsers($query), "username") : array();
+$template["movies"] = isset($query) ? omdb_search_movies_by_title($query) : array();
 
 require("template/base.php");
 
