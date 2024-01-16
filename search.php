61c37458b6f4dd22613fd83f94259450b67b369f
Giorgio Garofalo <giogar2705@gmail.com>
2024-01-16 12:18:02 +0100

    Allow searching for users

--- search.php
+++ search.php
@@ -6,6 +6,7 @@ $template["title"] = "Ricerca";
 $template["content"] = "search_content.php";
 
 $query = $_GET["s"];
+$template["users"] = array("user1", "user2", "user3"); // TODO query
 $template["movies"] = omdb_search_movies_by_title($query);
 
 require("template/base.php");
