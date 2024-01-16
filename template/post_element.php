d5fb7171866573b7ad23c4131b33d6002d805de8
Giorgio Garofalo <giogar2705@gmail.com>
2024-01-17 00:12:56 +0100

    Fix post comments

--- template/post_element.php
+++ template/post_element.php
@@ -1,10 +1,12 @@
 <?php
 $post = $_GET["post"];
-$comments = $dbh->getPostComments($post);
+$post_id = $post["post_id"];
+
+$comments = $dbh->getPostComments($post_id);
 $user_id = get_logged_in_username();
 $is_liked = $dbh -> alreadyLikedPost($user_id, $post);
 ?>
-<article class="post" id="<?php echo $post["post_id"]?>">
+<article class="post" id="<?php echo $post_id ?>">
     <header>
         <h3><?php echo $post["movie_title"]; ?></h3>
         <div class="stars stars-<?php echo $post["stars"] ?>"><svg></svg><svg></svg><svg></svg><svg></svg><svg></svg></div>            
@@ -15,13 +17,13 @@ $is_liked = $dbh -> alreadyLikedPost($user_id, $post);
             <li>
                 <a class="unliked <?php if ($is_liked) echo "liked" ?>">
                 <svg></svg>
-                <span><?php echo $dbh->getLikeCount($post["post_id"]) ?></span>
+                <span><?php echo $dbh->getLikeCount($post_id) ?></span>
                 </a>   
             </li>
             <li>
                 <a class="comment-button">
                 <svg></svg>
-                <span><?php echo $dbh->getCommentCount($post["post_id"]) ?></span>
+                <span><?php echo $dbh->getCommentCount($post_id) ?></span>
                 </a>
             </li>
             <li>
@@ -37,7 +39,7 @@ $is_liked = $dbh -> alreadyLikedPost($user_id, $post);
                 <p><?php echo $comment["body"]; ?></p>
             </div>
         <?php endforeach; ?>
-        <form action="api/create_comment.php?post_id=<?php echo $post["post_id"]?>" method="POST">
+        <form action="api/create_comment.php?post_id=<?php echo $post_id ?>" method="POST">
             <label for="write-comment" hidden>Scrivi un post</label>
             <textarea id="write-comment" name="write-comment" placeholder="Scrivi un commento..." rows="1"></textarea>
             <input type="submit" value="Invia">
