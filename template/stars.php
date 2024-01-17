03329559f972b2c20f378f34b37ad36126611233
Giorgio Garofalo <giogar2705@gmail.com>
2024-01-17 15:36:32 +0100

    Set stars amount as constant

--- template/stars.php
+++ template/stars.php
@@ -6,10 +6,12 @@ $filled_amount = $_GET["stars_filled_amount"] ?? 0;
 // application = user input, interactive
 // https://stackoverflow.com/a/49199809
 $svg_role = $_GET["stars_role"] ?? "document";
+
+const STARS_AMOUNT = 5;
 ?>
 
 <div class="stars stars-<?php echo $filled_amount ?>">
-    <?php for ($i = 0; $i < 5; $i++): ?>
+    <?php for ($i = 0; $i < STARS_AMOUNT; $i++): ?>
         <svg role="<?php echo $svg_role ?>">
             <title>Stella <?php echo $i + 1 ?></title>
         </svg>
