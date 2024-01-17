cc7dcff9bcbadd937b2690509fa873f7ef2ba1f4
Giorgio Garofalo <giogar2705@gmail.com>
2024-01-17 22:36:56 +0100

    Add no feed content placeholder

--- template/feed_content.php
+++ template/feed_content.php
@@ -1,12 +1,22 @@
+<?php
+
+$friends_movies = $template["friends_watched_movies"];
+$posts = $template["post"];
+
+?>
+
 <section class="col-left">
     <header>
         <h1>Attività di chi segui</h1>
     </header>
     <section>
         <h2>Visti di recente</h2>
+        <?php if (empty($friends_movies)): ?>
+            <p>Nessun contenuto da mostrare. Prova a seguire altri utenti!</p>
+        <?php endif ?>
         <ul class="movie-list">
             <?php
-            foreach ($template["friends_watched_movies"] as $movie) {
+            foreach ($friends_movies as $movie) {
                 $_GET["movieid"] = $movie["movie_id"];
                 require("movie_element.php");
             }
@@ -16,7 +26,10 @@
 </section>
 <section class="col-right">
     <h2>Post</h2>
-    <?php foreach ($template["post"] as $post){
+    <?php if (empty($friends_movies)): ?>
+        <p>Non ci sono nuovi post.</p>
+    <?php endif ?>
+    <?php foreach ($posts as $post) {
         $_GET["post"] = $post;
         require("post_element.php");
     }?>
