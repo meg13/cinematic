138677188b0ca172eaa3d60ee59fe0234effd7af
Margherita Raponi <raponi.margherita.rm@gmail.com>
2024-01-17 17:51:21 +0100

    added notification for like, comment and follow

--- api/like.php
+++ api/like.php
@@ -3,4 +3,6 @@ require("api.php");
 $user_id = get_logged_in_username();
 $post_id = $_GET["post_id"];
 $dbh->likePost($user_id, $post_id);
+$receiving_user_id = $dbh -> getPostUser($post_id);
+$dbh->newNotificationForPost($receiving_user_id, get_logged_in_username(), "L", $post_id);
 ?>
