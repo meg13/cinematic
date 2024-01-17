4a804c1cb61c6bd36add94084aebfa0b424eceef
Giorgio Garofalo <giogar2705@gmail.com>
2024-01-17 22:48:14 +0100

    Animate theme switch

--- notifications.php
+++ notifications.php
@@ -2,7 +2,7 @@
 
 require_once("bootstrap.php");
 
-$notifications = $dbh->getNotifications(get_logged_in_username());
+$notifications = $dbh->getNotifications("user2");
 
 $template["title"] = "Notifiche";
 $template["content"] = "notifications_content.php";
