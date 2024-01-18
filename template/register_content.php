c6f531dd110883dc65fa30b71d1f9deae0ae0d63
Margherita Raponi <raponi.margherita.rm@gmail.com>
2024-01-18 16:08:17 +0100

    change cursor for like and comment

--- template/register_content.php
+++ template/register_content.php
@@ -11,7 +11,7 @@
                 <input type="text" id="username" name="username"/>
             </li>
             <li><label for="email">E-mail</label>
-                <input type="text" id="email" name="email" placeholder="email@example.com"/>
+                <input type="email" id="email" name="email" placeholder="email@example.com"/>
             </li>
             <li><label for="password">Password</label>
                 <input type="password" id="password" name="password"/>
